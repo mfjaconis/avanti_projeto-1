a5c504379ccab26b2abd33516426d9d1f17f77f3
mfjaconis <matheusjaconis@gmail.com>
2025-04-23 11:33:38 -0300

    formatação do código

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,264 +1,268 @@
 const carouselsData = {
-    "carousel-1": [
-        {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },          
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },          
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-          {
-            tag: "Novo",
-            imageSrc: "./images/modelo_lancamento.png",
-            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-            originalPrice: "100,00",
-            offerPrice: "79,90",
-            offer: "10% OFF",
-            installment: "10x de R$ 7,90",
-          },
-    ],
-    "carousel-2": [
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-      {
-        tag: "Novo",
-        imageSrc: "./images/modelo_lancamento.png",
-        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
-        originalPrice: "100,00",
-        offerPrice: "79,90",
-        offer: "10% OFF",
-        installment: "10x de R$ 7,90",
-      },
-    ]
-  };
-  
-  const itemsPerPage = 5;
-  
-  function initializeCarousel(carouselId, data) {
-    let currentIndex = 0;
-  
-    const carouselElement = document.getElementById(carouselId);
-    const container = carouselElement.querySelector(".carousel-content");
-    const nextButton = carouselElement.querySelector(".carousel-content-button-next");
-    const backButton = carouselElement.querySelector(".carousel-content-button-back");
-    const bulletCircles = carouselElement.querySelectorAll(".bullet .circle");
-  
-    function renderCards() {
-      container.innerHTML = ""; 
-      const visibleCards = data.slice(currentIndex, currentIndex + itemsPerPage);
-  
-      for (const card of visibleCards) {
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("carousel-content-card");
-        cardElement.innerHTML = `
+	"carousel-1": [
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+	],
+	"carousel-2": [
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+		{
+			tag: "Novo",
+			imageSrc: "./images/modelo_lancamento.png",
+			description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+			originalPrice: "100,00",
+			offerPrice: "79,90",
+			offer: "10% OFF",
+			installment: "10x de R$ 7,90",
+		},
+	],
+};
+
+const itemsPerPage = 5;
+
+function initializeCarousel(carouselId, data) {
+	let currentIndex = 0;
+
+	const carouselElement = document.getElementById(carouselId);
+	const container = carouselElement.querySelector(".carousel-content");
+	const nextButton = carouselElement.querySelector(
+		".carousel-content-button-next",
+	);
+	const backButton = carouselElement.querySelector(
+		".carousel-content-button-back",
+	);
+	const bulletCircles = carouselElement.querySelectorAll(".bullet .circle");
+
+	function renderCards() {
+		container.innerHTML = "";
+		const visibleCards = data.slice(currentIndex, currentIndex + itemsPerPage);
+
+		for (const card of visibleCards) {
+			const cardElement = document.createElement("div");
+			cardElement.classList.add("carousel-content-card");
+			cardElement.innerHTML = `
         <div class="carousel-content-card-image">
           <p class="carousel-content-card-tag">${card.tag}</p>
           <img src="${card.imageSrc}" alt="${card.description}" class="carousel-content-card-image-person" />
@@ -273,42 +277,41 @@ const carouselsData = {
         </p>
         <button>Comprar</button>
       `;
-        container.appendChild(cardElement);
-      }
-  
-      updateBullets();
-    }
-  
-    function updateBullets() {
-      const totalPages = Math.ceil(data.length / itemsPerPage);
-      bulletCircles.forEach((circle, index) => {
-        if (index === Math.floor(currentIndex / itemsPerPage)) {
-          circle.classList.add("active");
-        } else {
-          circle.classList.remove("active");
-        }
-      });
-    }
-  
-    nextButton.addEventListener("click", () => {
-      const totalPages = Math.ceil(data.length / itemsPerPage);
-      if (currentIndex / itemsPerPage < totalPages - 1) {
-        currentIndex += itemsPerPage;
-        renderCards();
-      }
-    });
-  
-    backButton.addEventListener("click", () => {
-      if (currentIndex > 0) {
-        currentIndex -= itemsPerPage;
-        renderCards();
-      }
-    });
-  
-    renderCards();
-  }
-  
-  for (const carouselId of Object.keys(carouselsData)) {
-    initializeCarousel(carouselId, carouselsData[carouselId]);
-  }
-  
+			container.appendChild(cardElement);
+		}
+
+		updateBullets();
+	}
+
+	function updateBullets() {
+		const totalPages = Math.ceil(data.length / itemsPerPage);
+		bulletCircles.forEach((circle, index) => {
+			if (index === Math.floor(currentIndex / itemsPerPage)) {
+				circle.classList.add("active");
+			} else {
+				circle.classList.remove("active");
+			}
+		});
+	}
+
+	nextButton.addEventListener("click", () => {
+		const totalPages = Math.ceil(data.length / itemsPerPage);
+		if (currentIndex / itemsPerPage < totalPages - 1) {
+			currentIndex += itemsPerPage;
+			renderCards();
+		}
+	});
+
+	backButton.addEventListener("click", () => {
+		if (currentIndex > 0) {
+			currentIndex -= itemsPerPage;
+			renderCards();
+		}
+	});
+
+	renderCards();
+}
+
+for (const carouselId of Object.keys(carouselsData)) {
+	initializeCarousel(carouselId, carouselsData[carouselId]);
+}
--- scripts/search.js
+++ scripts/search.js
@@ -1,14 +1,14 @@
-const searchInput = document.getElementById('menu-search-input');
-const searchButton = document.getElementById('search-btn');
-const searchResult = document.getElementById('search-result');
+const searchInput = document.getElementById("menu-search-input");
+const searchButton = document.getElementById("search-btn");
+const searchResult = document.getElementById("search-result");
 
-searchButton.addEventListener('click', () => {
-    const searchText = searchInput.value.trim();
-    
-    if (searchText) {
-        searchResult.textContent = `Você buscou por: '${searchText}'`;
-        searchResult.style.display = 'block'; 
-    } else {
-        searchResult.style.display = 'none';
-    }
+searchButton.addEventListener("click", () => {
+	const searchText = searchInput.value.trim();
+
+	if (searchText) {
+		searchResult.textContent = `Você buscou por: '${searchText}'`;
+		searchResult.style.display = "block";
+	} else {
+		searchResult.style.display = "none";
+	}
 });
