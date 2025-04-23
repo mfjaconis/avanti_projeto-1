20ee5a784b241c3ede8aac76e915b8fb54d3c37e
mfjaconis <matheusjaconis@gmail.com>
2025-04-22 21:52:04 -0300

    carousel: adicionando interação ao carousel

---
+++ carousel.js
@@ -0,0 +1,283 @@
+const carouselsData = {
+    "carousel-1": [
+        {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+          {
+            tag: "Novo",
+            imageSrc: "./images/modelo_lancamento.png",
+            description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+            originalPrice: "100,00",
+            offerPrice: "79,90",
+            installment: "10x de R$ 7,90",
+          },
+    ],
+    "carousel-2": [
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+      {
+        tag: "Novo",
+        imageSrc: "./images/modelo_lancamento.png",
+        description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+        originalPrice: "100,00",
+        offerPrice: "79,90",
+        installment: "10x de R$ 7,90",
+      },
+    ]
+  };
+  
+  const itemsPerPage = 5;
+  
+  function initializeCarousel(carouselId, data) {
+    let currentIndex = 0;
+  
+    const carouselElement = document.getElementById(carouselId);
+    const container = carouselElement.querySelector(".carousel-content");
+    const nextButton = carouselElement.querySelector(".carousel-content-button-next");
+    const backButton = carouselElement.querySelector(".carousel-content-button-back");
+    const bulletCircles = carouselElement.querySelectorAll(".bullet .circle");
+  
+    function renderCards() {
+      container.innerHTML = ""; 
+      const visibleCards = data.slice(currentIndex, currentIndex + itemsPerPage);
+  
+      for (const card of visibleCards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("carousel-content-card");
+        cardElement.innerHTML = `
+          <div class="carousel-content-card-image">
+            <p class="carousel-content-card-tag">Novo</p>
+            <img src="${card.imageSrc}" alt="${card.description}" class="carousel-content-card-image-person" />
+          </div>
+          <h3 class="carousel-content-card-information">${card.description}</h3>
+          <span class="carousel-content-card-price">${card.originalPrice}</span>
+          <button>Comprar</button>
+        `;
+        container.appendChild(cardElement);
+      }
+  
+      updateBullets();
+    }
+  
+    // Atualização dos círculos indicadores (bullets)
+    function updateBullets() {
+      const totalPages = Math.ceil(data.length / itemsPerPage);
+      bulletCircles.forEach((circle, index) => {
+        if (index === Math.floor(currentIndex / itemsPerPage)) {
+          circle.classList.add("active");
+        } else {
+          circle.classList.remove("active");
+        }
+      });
+    }
+  
+    // Eventos de clique
+    nextButton.addEventListener("click", () => {
+      const totalPages = Math.ceil(data.length / itemsPerPage);
+      if (currentIndex / itemsPerPage < totalPages - 1) {
+        currentIndex += itemsPerPage;
+        renderCards();
+      }
+    });
+  
+    backButton.addEventListener("click", () => {
+      if (currentIndex > 0) {
+        currentIndex -= itemsPerPage;
+        renderCards();
+      }
+    });
+  
+    renderCards();
+  }
+  
+  for (const carouselId of Object.keys(carouselsData)) {
+    initializeCarousel(carouselId, carouselsData[carouselId]);
+  }
+  
